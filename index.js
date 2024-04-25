e1876a2e7c134984fa160b5ece0b3745eec7bb27
Mahdi Hasan Mahi <mh7266391@gmail.com>
2024-04-25 19:42:06 +0600

    started

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Coffee maker app is running");
+});
+
+app.listen(port, () => {
+  console.log(`Coffee server is running on port ${port}`);
+});
